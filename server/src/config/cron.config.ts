9cbe16e2bf303c2b547ffa49398de626d6ab2876
joshmasterton <joshmasterton@tuta.io>
2024-11-06 04:22:06 +0000

    Change upload time

--- server/src/config/cron.config.ts
+++ server/src/config/cron.config.ts
@@ -3,19 +3,19 @@ import { createBotComment, createBotPost } from "../bot/createPostComment.bot";
 import { likeDislikeBot } from "../bot/likeDislike.bot";
 
 export const scheduleRandomBotPost = () => {
-  cron.schedule("* * * * *", async () => {
+  cron.schedule("*/10 * * * *", async () => {
     await createBotPost();
   });
 };
 
 export const scheduleRandomBotComment = () => {
-  cron.schedule("* * * * *", async () => {
+  cron.schedule("*/10 * * * *", async () => {
     await createBotComment();
   });
 };
 
 export const scheduleRandomBotLikeDislike = () => {
-  cron.schedule("* * * * *", async () => {
+  cron.schedule("*/10 * * * *", async () => {
     await likeDislikeBot();
   });
 };
